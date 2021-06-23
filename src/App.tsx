c5a619d9e8397f4a7ccd9a070fe2bb7233e028de
Conrad <conradwcrawford@gmail.com>
2021-06-23 12:33:39 -0400

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import * as Icons from './components/Icons'
