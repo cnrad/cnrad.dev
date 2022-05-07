1a2d447f6ce7d0ab4ce67c56aa114e4ef0f490a3
cnrad <conradwcrawford@gmail.com>
2022-05-07 17:14:24 -0400

    change: _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,13 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const sheet = new ServerStyleSheet(),
-            originalRenderPage = ctx.renderPage;
-
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    // eslint-disable-next-line react/display-name
-                    enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-                });
-
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
-    }
+export default function Document() {
+    return (
+        <Html className="dark">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
