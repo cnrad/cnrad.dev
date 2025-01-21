cda432f6994dbc6d647bbf559e15665fc4bb6a4b
cnrad <conradwcrawford@gmail.com>
2025-01-21 02:08:23 -0500

    feat: basic art page

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -1,54 +1,106 @@
 import { Nav } from "@/components/Nav";
 import { PageContent } from "@/components/PageContent";
 import { PageWrapper } from "@/components/PageWrapper";
+import { Gallery } from "@/components/art/Gallery";
 
-export default function Home() {
+const COLLECTIONS = [
+  {
+    name: "SPHERUS",
+    description: "a collection of attempts to convey ineffable emotions.",
+    thumbnail: "/spherus-thumbnail.webp",
+    href: "https://spherus.cnrad.dev",
+  },
+  {
+    name: "FIGURA",
+    description:
+      "a collection of 12 beautiful, abstract wallpapers - all in stunning 6k resolution.",
+    thumbnail: "/figura.webp",
+    href: "https://cnrad.gumroad.com/l/figura",
+  },
+] as const;
+
+export default function Art() {
   return (
-    <PageWrapper>
-      <Nav />
+    <div className="h-full w-full flex flex-row">
+      <PageWrapper>
+        <Nav />
 
-      <PageContent key="art">
-        <div className="flex flex-col gap-6 text-sm h-full mt-6">
-          <div className="flex flex-col">
-            <h1 className="text-2xl mb-1 font-medium text-black">
-              Conrad Crawford
-            </h1>
-            <p className="mb-2">
-              digital abstract expressionist. exploring how color affects every
-              aspect of our lives.
-            </p>
-            <div className="flex flex-row gap-6">
-              <a
-                className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
-                href="https://unsplash.com/@cnrad"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                unsplash
-              </a>
-              <a
-                className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
-                href="https://x.com/notcnrad"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                twitter (x)
-              </a>
-              <a
-                className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                message
-              </a>
+        <PageContent key="art">
+          <div className="flex flex-col gap-6 text-sm h-full mt-6">
+            <div className="flex flex-col">
+              <h1 className="text-2xl mb-1 font-medium text-black">
+                Conrad Crawford
+              </h1>
+              <p className="mb-2">
+                digital abstract expressionist. exploring how color affects
+                every aspect of our lives.
+              </p>
+              <div className="flex flex-row gap-6">
+                <a
+                  className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
+                  href="https://unsplash.com/@cnrad"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  unsplash
+                </a>
+                <a
+                  className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
+                  href="https://x.com/notcnrad"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  twitter (x)
+                </a>
+                <a
+                  className="cursor-pointer text-[#A0A0A0] hover:text-black transition-colors duration-100"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  message
+                </a>
+              </div>
             </div>
-          </div>
 
-          <p className="font-medium italic text-black/15 mt-auto">
-            Conrad Crawford, {new Date().getFullYear()}
-          </p>
-        </div>
-      </PageContent>
-    </PageWrapper>
+            <div className="flex flex-col gap-3 max-w-sm">
+              <h3 className="font-bold leading-none">collections</h3>
+
+              {COLLECTIONS.map((collection) => (
+                <div className="flex flex-row gap-2">
+                  <img
+                    src={collection.thumbnail}
+                    alt={collection.name}
+                    className="rounded-sm w-16 h-full object-cover"
+                  />
+
+                  <div key={collection.name} className="flex flex-col">
+                    <h5 className="text-black font-medium">
+                      {collection.name}
+                    </h5>
+                    <p className="text-black/50 mb-2">
+                      {collection.description}
+                    </p>
+                    <a
+                      href={collection.href}
+                      className="text-black/60 w-min hover:text-black"
+                    >
+                      Visit
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <p className="font-medium italic text-black/15 mt-auto">
+              Conrad Crawford, {new Date().getFullYear()}
+            </p>
+          </div>
+        </PageContent>
+      </PageWrapper>
+
+      <div className="h-full w-full text-black text-sm px-24 leading-relaxed overflow-y-auto flex flex-col gap-4 py-24">
+        <Gallery />
+      </div>
+    </div>
   );
 }
---
+++ src/components/art/Gallery.tsx
@@ -0,0 +1,246 @@
+"use client";
+
+import Image from "next/image";
+
+// challenge TODO: scrape the heights of each and dynamically determine the order to make the total height of each column closest to equal
+const WORKS = [
+  {
+    name: "Ultrawide Minimalism (1)",
+    date: "06.03.24",
+    src: "ultrawide_minimalism_1.png",
+    post: "https://x.com/notcnrad/status/1797489177782845750",
+  },
+  {
+    name: "Ultrawide Minimalism (2)",
+    date: "06.03.24",
+    src: "ultrawide_minimalism_2.png",
+    post: "https://x.com/notcnrad/status/1797489177782845750",
+  },
+  {
+    name: "The Moment Our Eyes First Met",
+    date: "04.08.23",
+    src: "themomentoureyesfirstmet.png",
+  },
+  {
+    name: "Arcus Exploration",
+    date: "06.20.23",
+    src: "arcus.png",
+    post: "https://x.com/notcnrad/status/1671238076180430851",
+  },
+  {
+    name: "Arcus Exploration 2",
+    date: "06.21.23",
+    src: "arcus_background.png",
+    unsplash:
+      "https://unsplash.com/photos/a-dark-background-with-a-pattern-of-wavy-lines-EcWBgAdRqrQ",
+    post: "https://x.com/notcnrad/status/1671634017446141956",
+  },
+  {
+    name: "Fluctus",
+    date: "01.08.24",
+    src: "fluctus.png",
+  },
+  {
+    name: "Dawn of a New Tomorrow",
+    date: "09.11.23",
+    src: "dawnofanewtomorrow.png",
+    post: "https://x.com/notcnrad/status/1701223417372971378",
+  },
+  {
+    name: "Neo (Commission - Hop Inc.)",
+    date: "01.08.24",
+    src: "neo_hop.png",
+    post: "https://x.com/notcnrad/status/1641602543703736324",
+  },
+  {
+    name: "Neo",
+    date: "03.29.23",
+    src: "neo.png",
+    post: "https://x.com/notcnrad/status/1640931894933028870",
+  },
+  {
+    name: "Light Trail 1",
+    date: "01.08.24",
+    src: "light_trail_2.png",
+  },
+  {
+    name: "Light Trail 2",
+    date: "01.08.24",
+    src: "light_trail.png",
+  },
+  {
+    name: "Planet",
+    date: "05.09.23",
+    src: "planet.png",
+    unsplash:
+      "https://unsplash.com/photos/a-red-planet-with-a-black-background-LipGKRm7dBM",
+    post: "https://x.com/notcnrad/status/1655962324015124483",
+  },
+  {
+    name: "Rik Style",
+    date: "01.08.24",
+    src: "rik_style.png",
+    unsplash:
+      "https://unsplash.com/photos/a-purple-abstract-background-with-lines-and-curves-1uyWq9xVwcE",
+  },
+  {
+    name: "Spectrum",
+    date: "01.08.24",
+    src: "spectrum.jpg",
+    unsplash:
+      "https://unsplash.com/photos/a-black-background-with-a-multicolored-wave-of-light-cRoeAzZTWSc",
+  },
+  {
+    name: "To Gently Fall Further Away",
+    date: "01.08.24",
+    src: "togentlyfallfurtheraway.png",
+  },
+  {
+    name: "WWDC Exploration 1",
+    date: "06.05.23",
+    src: "wwdcexploration1.png",
+    unsplash:
+      "https://unsplash.com/photos/a-blue-abstract-background-with-curved-shapes-k3s7LZzX5xU",
+    featured: true,
+    post: "https://x.com/notcnrad/status/1665776662871527425",
+  },
+  {
+    name: "WWDC Exploration 2",
+    date: "06.05.23",
+    src: "wwdcexploration2.png",
+    unsplash:
+      "https://unsplash.com/photos/a-close-up-of-a-cell-phone-with-a-red-background-gg5lVy-Qlz8",
+    post: "https://x.com/notcnrad/status/1665776662871527425",
+  },
+  {
+    name: "WWDC Exploration 3",
+    date: "06.05.23",
+    src: "wwdcexploration3.png",
+    unsplash:
+      "https://unsplash.com/photos/a-close-up-of-a-cell-phone-with-a-green-and-blue-design-jm7hfafFt0g",
+    post: "https://x.com/notcnrad/status/1665776662871527425",
+  },
+  {
+    name: "Wallpaper Wednesday 1",
+    date: "04.23.23",
+    src: "wallpaper_wednesday_42323_1.png",
+    unsplash: "https://unsplash.com/photos/Utx0LfuC5Mk",
+  },
+  {
+    name: "Wallpaper Wednesday 2",
+    date: "04.23.23",
+    src: "wallpaper_wednesday_42323_2.png",
+    unsplash: "https://unsplash.com/photos/xzOSH_cUHFo",
+  },
+  {
+    name: "Wallpaper Wednesday 3",
+    date: "04.23.23",
+    src: "wallpaper_wednesday_42323_3.png",
+    unsplash: "https://unsplash.com/photos/mzzpvI2Z5r8",
+  },
+  {
+    name: "Wallpaper Wednesday 4",
+    date: "04.23.23",
+    src: "wallpaper_wednesday_42323_4.png",
+    unsplash: "https://unsplash.com/photos/SClBP10L2WI",
+  },
+  {
+    name: "Wallpaper Wednesday 5",
+    date: "04.23.23",
+    src: "wallpaper_wednesday_42323_5.png",
+    unsplash:
+      "https://unsplash.com/photos/a-black-and-white-photo-of-a-curved-object-3q9dlyY8CbI",
+    featured: true,
+  },
+  {
+    name: "Dimension",
+    date: "01.31.24",
+    post: "https://x.com/notcnrad/status/1752759331970285987",
+    src: "dimension.png",
+  },
+] as Array<{
+  name: string;
+  date: string;
+  src: string;
+  post?: string;
+  unsplash?: string;
+  featured?: boolean;
+}>;
+
+export const Gallery = () => {
+  return (
+    <div className="grid h-auto w-full grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      {Array(3)
+        .fill("")
+        .map((_val, i, arr) => (
+          <div key={i} className="relative flex h-auto w-full flex-col gap-4">
+            {WORKS.slice(
+              i * (WORKS.length / arr.length),
+              i * (WORKS.length / arr.length) + WORKS.length / arr.length
+            ).map((piece) => (
+              <ImageComponent key={piece.name} piece={piece} />
+            ))}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const ImageComponent = ({ piece }: { piece: (typeof WORKS)[number] }) => {
+  return (
+    <div className="group relative h-auto w-full cursor-pointer overflow-visible rounded-md">
+      <div
+        className="absolute bottom-0 z-[5] min-h-[min(100%,10rem)] w-full rounded-md transition-all duration-200 group-hover:bg-black/15 group-hover:backdrop-blur-md"
+        style={{ mask: "linear-gradient(transparent, black, black)" }}
+      />
+
+      <div className="absolute bottom-0 z-[10] flex min-h-16 w-full flex-col items-center justify-center gap-1 bg-transparent py-4 opacity-0 transition-all duration-200 group-hover:opacity-100">
+        <p className="text-sm font-semibold text-white/70">
+          {piece.name} {piece.featured ? "*" : null}
+        </p>
+        <a
+          href={piece.post ?? "https://x.com/notcnrad/"}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="flex flex-row text-sm font-medium text-white/30 hover:text-white/60"
+        >
+          {piece.date}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mb-auto ml-0.5 size-2"
+          >
+            <path d="M15 3h6v6" />
+            <path d="M10 14 21 3" />
+            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+          </svg>
+        </a>
+
+        <a
+          href={piece.unsplash ?? piece.src}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="mt-1 flex flex-row rounded-[4px] border border-white/10 bg-black/25 px-2 py-1 text-xs font-semibold text-white/75 transition-all duration-100 hover:border-white/15 hover:bg-black/50 hover:text-white"
+        >
+          View {piece.unsplash ? "on Unsplash" : null}
+        </a>
+      </div>
+
+      <Image
+        key={piece.name}
+        width={400}
+        height={400}
+        src={`/art/${piece.src}`}
+        alt={piece.name}
+        className="z-[10] h-fit w-full rounded-md object-cover"
+      />
+    </div>
+  );
+};
